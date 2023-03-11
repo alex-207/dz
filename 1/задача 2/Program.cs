52691ea8a717a37f30f969a349e13049c358d015
alex_207 <alex_207rm@mail.ru>
2023-03-11 20:45:33 +0300

    исправление 2/ задача 2

--- 1/задача 2/Program.cs
+++ 1/задача 2/Program.cs
@@ -1,16 +1,17 @@
 ﻿/*===========================================================================================================
 ПРОГРАММА НА ВХОД ПРИНИМАЕТ ДВА ЧИСЛА И ВЫДАЁТ, КАКОЕ ЧИСЛО БОЛЬШЕЕ, А КАКОЕ МЕНЬШЕЕ
 =============================================================================================================*/
-bool flag = false;
+bool flag1 = false;
+bool flag2 = false;
 int a = 0;
 int b = 0;
 Console.WriteLine($"Enter the first num: ");
-flag = int.TryParse(Console.ReadLine(), out a);  // считать строку и попытаться ее переформатировать в число и если удастся -> flag = true и записать это число в а
+flag1 = int.TryParse(Console.ReadLine(), out a);  // считать строку и попытаться ее переформатировать в число и если удастся -> flag = true и записать это число в а
 
 Console.WriteLine($"Enter the second num: ");
-flag = int.TryParse(Console.ReadLine(), out b);
+flag2 = int.TryParse(Console.ReadLine(), out b);
 
-if(flag == true) { // если введенные числа - корректны
+if(flag1 == true && flag2 == true) { // если введенные числа - корректны
     if(a > b) Console.WriteLine($"{a} > {b}");
     else if(a < b) Console.WriteLine($"{a} < {b}");
     else Console.WriteLine($"{a} = {b}");
