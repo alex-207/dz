99f504759b8a426ef7904479199a339149a08350
alex_207 <alex_207rm@mail.ru>
2023-03-11 22:20:47 +0300

    задача 8

--- 1/задача 8/Program.cs
+++ 1/задача 8/Program.cs
@@ -3,23 +3,44 @@
 =============================================================================================================*/
 bool flag = false;
 int N;
-int count = 1;
+int count = 2;
 
 Console.WriteLine($"Enter the num please: ");
 flag = int.TryParse(Console.ReadLine(), out N);  // считать строку и попытаться ее переформатировать в число и если удастся -> flag = true и записать это число в N
 
 if(flag == true) { // если введенное число - корректно
-    if(N >= 1) {
+    
+    // ЕСЛИ N - ЧЕТНОЕ, ПОЛОЖИТЕЛЬНОЕ
+    if(N > 0 && N % 2 == 0) {
         while(count <= N) {
             Console.Write($"{count} ");
-            count++;
+            count += 2;
         }
     }
 
-    else if(N < 1) {
-        while (count >= N) {
+    // ЕСЛИ N - НЕЧЕТНОЕ, ПОЛОЖИТЕЛЬНОЕ
+    else if(N > 0 && N % 2 != 0) {
+        while(count <= N) {
             Console.Write($"{count} ");
-            count--;
+            count += 2;
+        }
+    }
+
+    // ЕСЛИ N - ЧЕТНОЕ, ОТРИЦАТЕЛЬНОЕ
+    if(N < 0 && N % 2 == 0) {
+        count = 0;
+        while(count >= N) {
+            Console.Write($"{count} ");
+            count -= 2;
+        }
+    }
+
+    // ЕСЛИ N - НЕЧЕТНОЕ, ОТРИЦАТЕЛЬНОЕ
+    if(N < 0 && N % 2 != 0) {
+        count = 0;
+        while(count >= N) {
+            Console.Write($"{count} ");
+            count -= 2;
         }
     }
 }
