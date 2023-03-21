88a26fa4e7fe6b4583131f334a75752ceaf5d061
alex_207 <alex_207rm@mail.ru>
2023-03-21 17:59:04 +0300

    задача 25

---
+++ 4/задача 25/Program.cs
@@ -0,0 +1,20 @@
+﻿/*===========================================================================================================
+ПРОГРАММА ПРИНИМАЕТ НА ВХОД ДВА ЧИСЛА (a И b) И ЦИКЛИЧЕСКИ ВОЗВОДИТ ЧИСЛО a В НАТУРАЛЬНУЮ СТЕПЕНЬ b
+=============================================================================================================*/
+int CountDegree(int num, int degree)
+{
+    int result = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
+
+int a, b;
+Console.Write($"Введите число а: ");
+int.TryParse(Console.ReadLine(), out a);
+Console.Write($"Введите число b: ");
+int.TryParse(Console.ReadLine(), out b);
+
+Console.Write($"{a},{b} -> {CountDegree(a, b)}");
