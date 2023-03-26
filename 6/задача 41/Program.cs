15500a9300771c2d0f35809609c67364589e8fe1
alex_207 <alex_207rm@mail.ru>
2023-03-26 12:37:21 +0300

    задача 41

---
+++ 6/задача 41/Program.cs
@@ -0,0 +1,40 @@
+﻿/*===========================================================================================================
+ПРОГРАММА СЧИТЫВАЕТ С КЛАВИАТУРЫ ПОЛЬЗОВАТЕЛЬСКИЕ M ЧИСЕЛ И СЧИТАЕТ, СКОЛЬКО ЧИСЕЛ БОЛЬШЕ 0 ВВЁЛ ПОЛЬЗОВАТЕЛЬ
+=============================================================================================================*/
+#pragma warning disable
+
+void FillArr(int[] array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите число: ");
+        int.TryParse(Console.ReadLine(), out array[i]);
+    }
+}
+
+int Check(int[] array, int size)
+{
+    int result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+void PrintArr(int[] array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($"-> {Check(array, size)}");
+}
+
+int M = 5;
+int[] arr = new int[M];
+FillArr(arr, M);
+PrintArr(arr, M);
