ca19337e4c8f6c6166e320dd5a302f6673ce0df8
alex_207 <alex_207rm@mail.ru>
2023-03-26 12:39:40 +0300

    задача 43

---
+++ 6/задача 43/Program.cs
@@ -0,0 +1,24 @@
+﻿/*===========================================================================================================
+ПРОГРАММА ИЩЕТ ТОЧКУ ПЕРЕСЕЧЕНИЯ ДВУХ ПРЯМЫХ, ЗАДАННЫХ УРАВНЕНИЯМИ y = k1 * x + b1, y = k2 * x + b2; ЗНАЧЕНИЯ b1, k1, b2 И k2 ЗАДАЮТСЯ ПОЛЬЗОВАТЕЛЕМ
+=============================================================================================================*/
+#pragma warning disable
+
+double b1, k1, b2, k2, x, y;
+
+Console.Write($"Введите b1: ");
+double.TryParse(Console.ReadLine(), out b1);
+
+Console.Write($"Введите k1: ");
+double.TryParse(Console.ReadLine(), out k1);
+
+Console.Write($"Введите b2: ");
+double.TryParse(Console.ReadLine(), out b2);
+
+Console.Write($"Введите k2: ");
+double.TryParse(Console.ReadLine(), out k2);
+
+x = (b2 - b1) / (k1 - k2);
+y = k1 * x + b1;
+
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ");
+Console.Write($"({x} {y})");
